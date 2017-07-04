8afc411e99873384beba7c9c55a1d0e1b7158a9d
hanbyul_kim <rlagksquf30@gmail.com>
2017-07-04 12:15:40 +0000

    uri get request

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+    
+    def main
+        uri = URI('https://thingplugpf.sktiot.com:9443/0240771000000174/v1_0/remoteCSE-00000174d02544fffef01031/container-LoRa/latest')
+       # http = Net::HTTP.new(uri.host, uri.port)
+    #    http.use_ssl = true
+    #    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    #    @res = Net::HTTP::Get.new("https://thingplugpf.sktiot.com:9443/0240771000000174/v1_0/remoteCSE-00000174d02544fffef01031/container-LoRa/latest")
+
+
+        Net::HTTP.start(uri.host, uri.port) do |http|
+          request = Net::HTTP::Get.new(uri.request_uri)
+        
+          #request = http.request request # Net::HTTPResponse object
+        end
+        
+        @res=request
+
+
+    end
+end
