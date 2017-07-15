7679a1f5b9e6698a0fe07602287437021f6a6c6d
HanByulKim <rlagksquf30@gmail.com>
2017-07-16 03:28:01 +0900

    parser err

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,34 +1,35 @@
 class RequestController < ApplicationController
     skip_before_filter :verify_authenticity_token, :only => [:get_data]
     def get_data
-        notification_message = Nokogiri::XML(request.body.read)
-        puts(notification_message)
+        @doc = Nokogiri::XML(request.body.read)
+       	puts(@doc)
 
-        doc = Nokogiri::XML.parse(notification_message)
-        
         # parser
         # sty, ri, rn, pi, ct, lt, ppt<gwl, geui>, sr, et, st, cr, cnf, cs, con
-        ty = doc.css("ty")     # resource type as enum(3: container, 4: contentInstance, 7: execInstance, 12: mgmtCmd, 14: node, 16: remoteCSE)
-        ri = doc.css("ri")     # request identifier(리퀘스트 메시지 고유 식별자)
-        rn = doc.css("rn")     # resource name
-        pi = doc.css("pi")     
-        ct = doc.css("ct")
-        lt = doc.css("lt")     # last modified name of resource
-        ppt = doc.css("ppt")
-        gwl = doc.css("gwl")   # seems latlng
-        values_g = gwl.split(",")
+        ty = @doc.css('ty')     # resource type as enum(3: container, 4: contentInstance, 7: execInstance, 12: mgmtCmd, 14: node, 16: remoteCSE)
+        ri = @doc.css('ri')     # request identifier(리퀘스트 메시지 고유 식별자)
+        rn = @doc.css('rn')     # resource name
+        pi = @doc.css('pi')     
+        ct = @doc.css('ct')
+        lt = @doc.css('lt')     # last modified name of resource
+	puts('lt : ',lt)
+        ppt = @doc.css('ppt')
+        gwl = @doc.css('gwl')   # seems latlng
+	puts('gwl : ', gwl)
+        values_g = gwl.text.split(",")
+	puts('valg : ', values_g[0])
         lat = values_g[0].to_f
         lng = values_g[1].to_f
-        geui = doc.css("geui")
-        sr = doc.css("sr")      # request uri
-        values_u = sr.split("/")
+        geui = @doc.css('geui')
+        sr = @doc.css('sr')      # request uri
+        values_u = sr.text.split("/")
         dev_id = values_u[3]
-        et = doc.css("et")     # expriation time(리소스 유효기간)
-        st = doc.css("st")     # state tag 
-        cr = doc.css("cr")     # data sender indentifier(데이터 고유 식별자)
-        cnf = doc.css("cnf")   # data type
-        cs = doc.css("cs")     # content size
-        con = doc.css("con")   # content(raw data)
+        et = @doc.css('et')     # expriation time(리소스 유효기간)
+        st = @doc.css('st')     # state tag 
+        cr = @doc.css('cr')     # data sender indentifier(데이터 고유 식별자)
+        cnf = @doc.css('cnf')   # data type
+        cs = @doc.css('cs')     # content size
+        con = @doc.css('con')   # content(raw data)
 
         puts('lat : ', lat)
         puts('lng : ', lng)
