fce3d799d57b9139ed1514b02c18b9672b2f645a
HanByulKim <rlagksquf30@gmail.com>
2017-07-15 14:45:59 +0900

    loc

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,6 +1,8 @@
 class RequestController < ApplicationController
+    skip_before_filter :verify_authenticity_token, :only => [:get_data]
     def get_data
         notification_message = Nokogiri::XML(request.body.read)
+        puts(notification_message)
         Req.create(content: notification_message)
     end
 end
