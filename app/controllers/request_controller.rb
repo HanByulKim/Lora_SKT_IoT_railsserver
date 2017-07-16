4291df251a6f34c724f92661fb4200cc2959344d
HanByulKim <rlagksquf30@gmail.com>
2017-07-16 14:49:31 +0900

    parser var type

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -6,35 +6,53 @@ class RequestController < ApplicationController
 
         # parser
         # sty, ri, rn, pi, ct, lt, ppt<gwl, geui>, sr, et, st, cr, cnf, cs, con
-        ty = @doc.css('ty')     # resource type as enum(3: container, 4: contentInstance, 7: execInstance, 12: mgmtCmd, 14: node, 16: remoteCSE)
+        ty_r = @doc.css('ty')     # resource type as enum(3: container, 4: contentInstance, 7: execInstance, 12: mgmtCmd, 14: node, 16: remoteCSE)
+	ty = ty_r[0].text.split(">")
+	tya = ty[0].to_i
+	puts('ty : ',tya)
         ri = @doc.css('ri')     # request identifier(리퀘스트 메시지 고유 식별자)
+	ri = ri[0].text.split(">")
         rn = @doc.css('rn')     # resource name
+	rn = rn[0].text.split(">")
         pi = @doc.css('pi')     
+	pi = pi[0].text.split(">")
         ct = @doc.css('ct')
+	ct = ct[0].text.split(">")
         lt = @doc.css('lt')     # last modified name of resource
+	lt = lt[0].text.split(">")
 	puts('lt : ',lt)
         ppt = @doc.css('ppt')
-        gwl = @doc.css('gwl')   # seems latlng
+	ppt = ppt[0].text.split(">")
+	puts('ppt : ',ppt)
+        gwl_r = @doc.css('gwl')   # seems latlng
+	gwl = gwl_r[0].text.split(">")
 	puts('gwl : ', gwl)
-        values_g = gwl.text.split(",")
+        values_g = gwl_r.text.split(",")
 	puts('valg : ', values_g[0])
         lat = values_g[0].to_f
         lng = values_g[1].to_f
         geui = @doc.css('geui')
-        sr = @doc.css('sr')      # request uri
-        values_u = sr.text.split("/")
+	geui = geui[0].text.split(">")
+        sr_r = @doc.css('sr')      # request uri
+	sr = sr_r[0].text.split(">")
+        values_u = sr_r.text.split("/")
         dev_id = values_u[3]
         et = @doc.css('et')     # expriation time(리소스 유효기간)
+	et = et[0].text.split(">")
         st = @doc.css('st')     # state tag 
+	st = st[0].text.split(">")
         cr = @doc.css('cr')     # data sender indentifier(데이터 고유 식별자)
+	cr = cr[0].text.split(">")
         cnf = @doc.css('cnf')   # data type
+	cnf = cnf[0].text.split(">")
         cs = @doc.css('cs')     # content size
+	cs = cs[0].text.split(">")
         con = @doc.css('con')   # content(raw data)
+	con = con[0].text.split(">")
 
         puts('lat : ', lat)
         puts('lng : ', lng)
         puts('dev_id : ', dev_id)
 
-        Req.create(content: notification_message)
     end
 end
--- db/migrate/20170714200530_create_reqs.rb
+++
@@ -1,9 +0,0 @@
-class CreateReqs < ActiveRecord::Migration[5.0]
-  def change
-    create_table :reqs do |t|
-      t.string :content
-
-      t.timestamps
-    end
-  end
-end
