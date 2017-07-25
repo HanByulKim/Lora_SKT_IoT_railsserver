e22b31b574fb09c35bb091d09fe3e61d73f54039
HanByulKim <rlagksquf30@gmail.com>
2017-07-26 06:32:01 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   get '/home/main' => 'home#main'
+root 'home#story'
   get '/home/story' => 'home#story'
   post '/data_receiver' => 'request#get_data'
   # The priority is based upon order of creation: first created -> highest priority.
